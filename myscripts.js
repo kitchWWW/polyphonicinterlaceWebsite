d1c8dd10f61079507a991fbf741da7441d37470f
Brian Ellis <bse@fb.com>
2021-06-30 02:30:00 -0400

    yup

--- myscripts.js
+++ myscripts.js
@@ -3,26 +3,22 @@ const not_started_text = "Play Audio"
 const resume_text = "Resume Audio"
 
 
-document.getElementById("playbutton").innerHTML=not_started_text;
-
+document.getElementById("myPlaybackDiv").style.display = "none";
 
 var whichToUse = Math.floor(Math.random() * 3);
+var audioPath = 'res/websiteTrack'+(whichToUse+1)+'.mp3';
 
-var audio = new Audio('res/websiteTrack'+(whichToUse+1)+'.mp3');
+var playingAtAll = false
 
 function buttonPressed()  {
-	var status = document.getElementById("playbutton").innerHTML
-	if(status === not_started_text){
-		audio.play();
-		document.getElementById("playbutton").innerHTML = playing_text;
-	}else if (status ===playing_text){
-		audio.pause();
-		document.getElementById("playbutton").innerHTML = resume_text;
-	}else if(status === resume_text){
-		audio.play()
-		document.getElementById("playbutton").innerHTML = playing_text;
+	if(playingAtAll){
+		return;
 	}
-
-
+	playingAtAll = true
+	document.getElementById("myAudio").setAttribute('src', audioPath);
+	document.getElementById("myPlaybackDiv").style.display = "block";
+	document.getElementById("myAudio").play()
+	console.log("playing!");
 }
 
+console.log("it is going");
